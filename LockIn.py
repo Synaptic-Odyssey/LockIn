60dbc1804343b75c086e6702ba956a3afaeab88d
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-05-25 16:25:46 -0700

    it works

--- LockIn.py
+++ LockIn.py
@@ -3,6 +3,7 @@ from pywinauto import Desktop
 from win32gui import FindWindow, PostMessage
 import win32.lib.win32con as win32con
 from calendarevent import *
+import time
 def get_current_calander_event():
     return "Coding"
 def get_categories():
@@ -13,7 +14,7 @@ def categorize_events(calendar_event):
     return response['response'].replace(" ","").split(",")
 def get_category_list(categories):
     # return 
-    trainstr="Please respond with the relevance of a window title to the category and current calendar event. Measure relevance on a continuous 0-10 scale with 0 being completly irrelevant and 10 being extremely relevant. I have attached some examples of window title and relevance for the current categories. The current categories are "+ str(categories)+"Examples:"
+    trainstr="Please respond with the relevance of a window title to the category. Take a narrow defenition of the category. Measure relevance on a continuous 0-10 scale with 0 being completly irrelevant and 10 being extremely relevant.Dont give something a score about a 7 if it isnt related to the category I have attached some examples of window title and relevance for the current categories. The current categories are "+ str(categories)+"Examples:"
     for category in categories:
         cat_file=open(".\\Categories\\"+category+".txt", 'r')
         lines=cat_file.readlines()
@@ -27,12 +28,12 @@ def eval_windows(trainstr,calendar_event):
     for w in windows:
         if(len(w.window_text())>3):    
             print(w.window_text())
-            response=ollama.generate(model="llama3", prompt=trainstr+" Please answer just like the examples and take into account the task"+calendar_event+" without any extra text or explaination,just the relevance. Here is the window title: "+w.window_text())
+            response=ollama.generate(model="llama3", prompt=trainstr+" Please answer with the relevance and take into account the task"+calendar_event+"  WITHGOU ANY EXTRA TEXT OR EXPLAINATION (or else),just the relevance as a number. Here is the window title: "+w.window_text())
             print(response['response'])
             try:
                 
                 if(int(response['response'])<=3 and w.window_text()!="Taskbar"):
-                    w.minimize()
+                    w.close()
             except:
                 pass
             
@@ -40,13 +41,13 @@ def eval_windows(trainstr,calendar_event):
 
 def learn(categories):
     windows = Desktop(backend="uia").windows()
-    trainstr="Please respond with the relevance of a window title to the category. Take a narrow defemnition of the category. Measure relevance on a continuous 0-10 scale with 0 being completly irrelevant and 10 being extremely relevant. I have attached some examples of window title and relevance for the current categories. The current categories are "+ str(categories)+"Examples:"
+    trainstr="Please respond with the relevance of a window title to the category. Take a narrow defenition of the category. Measure relevance on a continuous 0-10 scale with 0 being completly irrelevant and 10 being extremely relevant.Dont give something a score about a 7 if it isnt related to the category I have attached some examples of window title and relevance for the current categories. The current categories are "+ str(categories)+"Examples:"
     for category in categories:
         cat_file=open(".\\Categories\\"+category+".txt", 'r')
         lines=cat_file.readlines()
         for line in lines:
             trainstr+=line
-    print(trainstr)
+    # print(trainstr)
     for w in windows:
         if(len(w.window_text())>3):    
             print(w.window_text())
@@ -60,9 +61,12 @@ def learn(categories):
             cat_file.write(w.window_text()+","+str(val)+"\n")
 
 
-
-# eval_windows(get_category_list(categorize_events("Coding Project"))," Coding Project")
-learn(categorize_events(get_event("https://calendar.google.com/calendar/ical/1e3a81e23db15db5371aff83435e626e996ef8f26f75b1331ab17ec0efab37ba%40group.calendar.google.com/private-cd3778bb335f55bf428dc4307e5a7994/basic.ics")))
+event=get_event("https://calendar.google.com/calendar/ical/1e3a81e23db15db5371aff83435e626e996ef8f26f75b1331ab17ec0efab37ba%40group.calendar.google.com/private-cd3778bb335f55bf428dc4307e5a7994/basic.ics")
+while True:
+    
+    eval_windows(get_category_list(categorize_events(event)),event)
+    time.sleep(5)
+# learn(categorize_events(event))
 # def switch_tasks():
 #     calandar_event=get_current_calander_event()
 #     category=categorize_event(calandar_event)
